c5d1e0877caa4ce6e2e953be78c9c1b7ce9274a0
bjoern.kinberger <bkinberger.brk@gmail.com>
2024-04-02 16:44:30 +0200

    [scanner] - test scanner in ui

--- app/src/main/java/bjoern/kinberger/gcx/grandcentrix_uwb_ble_android/MainActivity.kt
+++ app/src/main/java/bjoern/kinberger/gcx/grandcentrix_uwb_ble_android/MainActivity.kt
@@ -3,14 +3,21 @@ package bjoern.kinberger.gcx.grandcentrix_uwb_ble_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import bjoern.kinberger.gcx.grandcentrix_uwb_ble_android.ui.theme.GrandcentrixuwbbleandroidTheme
+import org.koin.androidx.compose.getViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +27,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    ScanScreen()
                 }
             }
         }
@@ -30,17 +37,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ScanScreen(viewModel: MainActivityViewModel = getViewModel()) {
+    val viewState by viewModel.viewState.collectAsState()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GrandcentrixuwbbleandroidTheme {
-        Greeting("Android")
+    Column {
+        Row {
+            Button(onClick = { viewModel.scan() }) {
+                Text(text = "Start scan")
+            }
+        }
+        viewState.results.forEach {result -> 
+            Text(text = "Address: ${result.device.address}")
+        }
     }
 }
