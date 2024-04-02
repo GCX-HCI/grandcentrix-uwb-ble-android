52f153c97f3c5c29e5327d447eb34a0d2ae7ed9b
bjoern.kinberger <bkinberger.brk@gmail.com>
2024-04-02 11:49:24 +0200

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "grandcentrix-uwb-ble-android"
+include(":app")
