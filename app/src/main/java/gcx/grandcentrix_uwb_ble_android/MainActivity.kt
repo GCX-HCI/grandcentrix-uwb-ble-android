d0df40f32d5c8ea692e44dcab2675ee796884008
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-04-04 16:41:03 +0200

    [connecting] - test new connecting method in ui

--- app/src/main/java/gcx/grandcentrix_uwb_ble_android/MainActivity.kt
+++ app/src/main/java/gcx/grandcentrix_uwb_ble_android/MainActivity.kt
@@ -1,5 +1,6 @@
 package gcx.grandcentrix_uwb_ble_android
 
+import android.bluetooth.BluetoothDevice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,12 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import gcx.ble.manager.ConnectionState
 import gcx.grandcentrix_uwb_ble_android.ui.theme.GrandcentrixuwbbleandroidTheme
 import org.koin.androidx.compose.getViewModel
 
@@ -42,7 +45,25 @@ fun ScanScreen(viewModel: MainActivityViewModel = getViewModel()) {
             Text(text = "Start scan")
         }
         viewState.results.forEach { result ->
-            Text(text = "Address: ${result.device.address}")
+            DeviceItem(
+                device = result.device,
+                onItemClicked = viewModel::connectToDevice,
+                connectionState = viewState.connectionState,
+            )
+        }
+    }
+}
+
+@Composable
+fun DeviceItem(
+    device: BluetoothDevice,
+    onItemClicked: (BluetoothDevice) -> Unit,
+    connectionState: ConnectionState,
+) {
+    OutlinedButton(onClick = { onItemClicked(device) }) {
+        Column {
+            Text(text = "Address: ${device.address}")
+            Text(text = "Connection state: ${connectionState.name}")
         }
     }
 }
--- app/src/main/java/gcx/grandcentrix_uwb_ble_android/MainActivityViewModel.kt
+++ app/src/main/java/gcx/grandcentrix_uwb_ble_android/MainActivityViewModel.kt
@@ -1,10 +1,14 @@
 package gcx.grandcentrix_uwb_ble_android
 
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.le.ScanResult
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import gcx.ble.manager.BleManager
+import gcx.ble.manager.ConnectionState
 import gcx.ble.scanner.BleScanner
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,12 +19,15 @@ import kotlinx.coroutines.launch
 
 data class MainActivityViewState(
     val results: List<ScanResult> = mutableListOf(),
+    val connectionState: ConnectionState = ConnectionState.DISCONNECTED,
 )
 
 private const val mobileKnowledgeAddress = "00:60:37:90:E7:11"
 private const val TAG = "MainActivityViewModel"
+
 class MainActivityViewModel(
     private val bleScanner: BleScanner,
+    private val bleManager: BleManager,
 ) : ViewModel() {
     private val _viewState = MutableStateFlow(MainActivityViewState())
     val viewState: StateFlow<MainActivityViewState> = _viewState.asStateFlow()
@@ -37,6 +44,20 @@ class MainActivityViewModel(
                         val newResults = listOf(result)
                         it.copy(
                             results = it.results + newResults,
+                        )
+                    }
+                    cancel()
+                }
+        }
+    }
+
+    fun connectToDevice(bleDevice: BluetoothDevice) {
+        viewModelScope.launch {
+            bleManager.connect(bleDevice)
+                .collect { connectionState ->
+                    _viewState.update {
+                        it.copy(
+                            connectionState = connectionState,
                         )
                     }
                 }
