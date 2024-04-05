86613e3c06ba73e6a908b5ef86fc59171954afbb
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-04-05 12:35:30 +0200

    [connecting] - disconnect in the await close

--- core/ble/src/main/java/gcx/ble/manager/GcxBleManager.kt
+++ core/ble/src/main/java/gcx/ble/manager/GcxBleManager.kt
@@ -68,6 +68,7 @@ class GcxBleManager(
                     )
 
                 awaitClose {
+                    gatt.disconnect()
                     gatt.close()
                 }
             } catch (exception: SecurityException) {
