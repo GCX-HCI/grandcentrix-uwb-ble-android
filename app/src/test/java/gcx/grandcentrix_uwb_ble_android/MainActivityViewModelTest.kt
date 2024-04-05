0155cafa1cde06c7af797c16e0a195e498d5adf1
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-04-05 13:27:03 +0200

    [connecting] - add some unit test for our VM

--- app/src/test/java/gcx/grandcentrix_uwb_ble_android/MainActivityViewModelTest.kt
+++ app/src/test/java/gcx/grandcentrix_uwb_ble_android/MainActivityViewModelTest.kt
@@ -1,7 +1,9 @@
 package gcx.grandcentrix_uwb_ble_android
 
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.le.ScanResult
 import gcx.ble.manager.BleManager
+import gcx.ble.manager.ConnectionState
 import gcx.ble.scanner.BleScanner
 import gcx.test.CoroutineTestExtension
 import io.mockk.every
@@ -10,6 +12,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -23,6 +26,8 @@ class MainActivityViewModelTest {
     private val bleScanner: BleScanner = mockk {
         every { startScan() } returns flowOf(scanResult)
     }
+
+    private val bluetoothDevice: BluetoothDevice = mockk()
 
     private val bleManager: BleManager = mockk()
 
@@ -49,5 +54,57 @@ class MainActivityViewModelTest {
 
             val viewState = viewModel.viewState.value
             assert(viewState.results.isEmpty())
+        }
+
+    @Test
+    fun `Given an ble device, when connect to device, then ble is connected`() =
+        runTest {
+
+            every { bleManager.connect(bluetoothDevice) } returns flowOf(ConnectionState.CONNECTED)
+
+            val viewModel = MainActivityViewModel(bleScanner, bleManager)
+            viewModel.connectToDevice(bluetoothDevice)
+
+            advanceUntilIdle()
+
+            val viewState = viewModel.viewState.value
+            Assertions.assertEquals(
+                ConnectionState.CONNECTED,
+                viewState.connectionState.second
+            )
+        }
+
+    @Test
+    fun `Given no ble device, when get connection state for device, then connection state is DISCONNECTED`() =
+        runTest {
+
+            val viewModel = MainActivityViewModel(bleScanner, bleManager)
+            viewModel.getConnectionStateForDevice(bluetoothDevice)
+
+            advanceUntilIdle()
+
+            val viewState = viewModel.viewState.value
+            Assertions.assertEquals(
+                ConnectionState.DISCONNECTED,
+                viewState.connectionState.second
+            )
+        }
+
+    @Test
+    fun `Given ble device, when get connection state for device, then return connection state of the device `() =
+        runTest {
+
+            every { bleManager.connect(bluetoothDevice) } returns flowOf(ConnectionState.READY)
+
+            val viewModel = MainActivityViewModel(bleScanner, bleManager)
+            viewModel.connectToDevice(bluetoothDevice)
+
+            advanceUntilIdle()
+
+            val result = viewModel.getConnectionStateForDevice(bluetoothDevice)
+            Assertions.assertEquals(
+                ConnectionState.READY,
+                result
+            )
         }
 }
