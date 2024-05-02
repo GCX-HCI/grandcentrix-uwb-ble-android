5f4c3cbee570312fa9deaddcc217c1f26d81194b
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-05-02 14:19:03 +0200

    [start-session] - improve ext function for better readability

--- core/uwb/src/main/java/net/grandcentrix/uwb/ext/String.kt
+++ core/uwb/src/main/java/net/grandcentrix/uwb/ext/String.kt
@@ -1,18 +1,9 @@
 package net.grandcentrix.uwb.ext
 
 fun String.hexStringToByteArray(): ByteArray {
-    val len = this.length
-    val data = ByteArray(len / 2)
-    var i = 0
-    while (i < len) {
-        data[i / 2] =
-            (
-                (
-                    this[i].digitToIntOrNull(16)
-                        ?: (-1 shl 4)
-                    ) + this[i + 1].digitToIntOrNull(16)!!
-                ).toByte()
-        i += 2
-    }
-    return data
+    check(length % 2 == 0) { "Must have an even length" }
+
+    return chunked(2)
+        .map { it.toInt(16).toByte() }
+        .toByteArray()
 }
