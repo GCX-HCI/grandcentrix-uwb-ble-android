738f5b35d4854245c3c3e7bec5afb888a44f43e6
Fabian Vorholt <fabian.vorholt@grandcentrix.net>
2024-05-06 13:21:01 +0200

    Request UWB ranging permission in ranging screen if needed

--- app/src/main/java/net/grandcentrix/uwbBleAndroid/di/MainModule.kt
+++ app/src/main/java/net/grandcentrix/uwbBleAndroid/di/MainModule.kt
@@ -19,7 +19,8 @@ val mainModule = module {
     viewModel {
         RangingViewModel(
             uwbBleLibrary = get(),
-            navigator = get()
+            navigator = get(),
+            permissionChecker = get()
         )
     }
 
