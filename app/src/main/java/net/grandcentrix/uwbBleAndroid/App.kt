650b6f6e98a9596d5ba4a819c214f37fd4ba6625
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-05-06 16:06:21 +0200

    [object-creation] - remove koin from lib project

--- app/src/main/java/net/grandcentrix/uwbBleAndroid/App.kt
+++ app/src/main/java/net/grandcentrix/uwbBleAndroid/App.kt
@@ -1,8 +1,6 @@
 package net.grandcentrix.uwbBleAndroid
 
 import android.app.Application
-import net.grandcentrix.data.di.dataModule
-import net.grandcentrix.data.di.provideSubModules
 import net.grandcentrix.uwbBleAndroid.di.mainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -15,9 +13,8 @@ class App : Application() {
 
             modules(
                 listOf(
-                    mainModule,
-                    dataModule
-                ) + provideSubModules
+                    mainModule
+                )
             )
         }
     }
