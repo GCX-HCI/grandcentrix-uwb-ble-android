d4d3b723f7a0855d5f84196bbdc2fbce72ab4bf2
Fabian Vorholt <30832848+fvorholt@users.noreply.github.com>
2024-05-13 11:54:50 +0200

    [Refactoring] Run UWB start up sequentially (#73)

--- core/uwb/src/main/java/net/grandcentrix/uwb/exception/UwbException.kt
+++ core/uwb/src/main/java/net/grandcentrix/uwb/exception/UwbException.kt
@@ -2,5 +2,5 @@ package net.grandcentrix.uwb.exception
 
 sealed class UwbException(override val message: String?) : Exception() {
 
-    data object InitialisationFailure : UwbException("Failed to initialize UWB ranging")
+    data object DeciveConfigNullException : UwbException("Didn't receive compliant device config")
 }
