048e12cbaf41df9e925851e7f64a54a7544dd5e7
Fabian Vorholt <fabian.vorholt@grandcentrix.net>
2024-05-14 16:09:15 +0200

    Add internal logger class to filter by log level

---
+++ lib/src/main/java/net/grandcentrix/api/logging/internal/UwbLogger.kt
@@ -0,0 +1,49 @@
+package net.grandcentrix.api.logging.internal
+
+import net.grandcentrix.api.logging.LogConfig
+import net.grandcentrix.api.logging.LogLevel
+import net.grandcentrix.api.logging.Logger
+
+/**
+ * Internal logger class. Filters log output by [level].
+ */
+internal class UwbLogger(
+    private val level: LogLevel,
+    private val logger: Logger
+) {
+    internal fun logV(tag: String, message: String, throwable: Throwable? = null) {
+        if (LogLevel.Verbose.ordinal >= level.ordinal) {
+            logger.v(tag, message, throwable)
+        }
+    }
+
+    internal fun logD(tag: String, message: String, throwable: Throwable? = null) {
+        if (LogLevel.Debug.ordinal >= level.ordinal) {
+            logger.d(tag, message, throwable)
+        }
+    }
+
+    internal fun logI(tag: String, message: String, throwable: Throwable? = null) {
+        if (LogLevel.Info.ordinal >= level.ordinal) {
+            logger.i(tag, message, throwable)
+        }
+    }
+
+    internal fun logW(tag: String, message: String, throwable: Throwable? = null) {
+        if (LogLevel.Warning.ordinal >= level.ordinal) {
+            logger.w(tag, message, throwable)
+        }
+    }
+
+    internal fun logE(tag: String, message: String, throwable: Throwable? = null) {
+        if (LogLevel.Error.ordinal >= level.ordinal) {
+            logger.e(tag, message, throwable)
+        }
+    }
+
+    companion object {
+        fun initialize(logConfig: LogConfig): UwbLogger {
+            return UwbLogger(logConfig.level, logConfig.logger)
+        }
+    }
+}
