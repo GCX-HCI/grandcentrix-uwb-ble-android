e8e96b3dcf6853c9a1949202b2dcc8c27dea4f24
Fabian Vorholt <fabian.vorholt@grandcentrix.net>
2024-05-14 16:08:52 +0200

    Add customizable logger implementation

---
+++ lib/src/main/java/net/grandcentrix/api/logging/LogLevel.kt
@@ -0,0 +1,12 @@
+package net.grandcentrix.api.logging
+
+/**
+ * Log levels used in the library. Log messages are filtered by [LogConfig.level].
+ */
+enum class LogLevel {
+    Verbose,
+    Debug,
+    Info,
+    Warning,
+    Error
+}
