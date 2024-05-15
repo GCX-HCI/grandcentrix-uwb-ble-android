f8f7a369889b354774527fc9b2f45d03094997da
Fabian Vorholt <fabian.vorholt@grandcentrix.net>
2024-05-15 16:51:32 +0200

    Pass uwb device to ranging screen and include in default parameter

--- app/src/main/java/net/grandcentrix/uwbBleAndroid/MainActivity.kt
+++ app/src/main/java/net/grandcentrix/uwbBleAndroid/MainActivity.kt
@@ -7,17 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import net.grandcentrix.uwbBleAndroid.ui.Navigator
 import net.grandcentrix.uwbBleAndroid.ui.Screen
 import net.grandcentrix.uwbBleAndroid.ui.ble.BleScreen
 import net.grandcentrix.uwbBleAndroid.ui.ranging.RangingScreen
-import net.grandcentrix.uwbBleAndroid.ui.ranging.RangingViewModel
 import net.grandcentrix.uwbBleAndroid.ui.theme.AppTheme
 import org.koin.android.ext.android.inject
-import org.koin.androidx.compose.koinViewModel
-import org.koin.core.parameter.parametersOf
 
 class MainActivity : ComponentActivity() {
 
@@ -34,20 +30,12 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     // Very basic navigation implementation
-                    val currentScreen by navigator.currentScreen.collectAsState()
+                    val currentScreenState = navigator.currentScreen.collectAsState()
 
-                    when (currentScreen) {
+                    when (val currentScreen = currentScreenState.value) {
                         Screen.Connect -> BleScreen()
                         is Screen.Ranging -> {
-                            val viewModel: RangingViewModel =
-                                koinViewModel(
-                                    parameters = {
-                                        parametersOf(
-                                            (currentScreen as Screen.Ranging).uwbDevice
-                                        )
-                                    }
-                                )
-                            RangingScreen(viewModel = viewModel)
+                            RangingScreen(uwbDevice = currentScreen.uwbDevice)
                         }
                     }
                 }
