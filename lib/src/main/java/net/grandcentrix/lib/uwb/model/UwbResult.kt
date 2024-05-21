6a1a35e61cde291ee0fe7521fe8a9a885f15bada
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-05-21 14:58:49 +0200

    [emit-uwb-controller-messages] - add a new result model what handles different result messages from our controller and session

---
+++ lib/src/main/java/net/grandcentrix/lib/uwb/model/UwbResult.kt
@@ -0,0 +1,28 @@
+package net.grandcentrix.lib.uwb.model
+
+import androidx.core.uwb.RangingMeasurement
+import androidx.core.uwb.RangingResult
+
+sealed interface UwbResult {
+    data object RangingStarted : UwbResult
+
+    data object RangingStopped : UwbResult
+
+    data class PositionResult(
+        val distance: RangingMeasurement?,
+        val azimuth: RangingMeasurement?,
+        val elevation: RangingMeasurement?,
+        val elapsedRealtimeNanos: Long
+    ) : UwbResult
+
+    data object Disconnected : UwbResult
+
+    data object UnknownResult : UwbResult
+}
+
+fun RangingResult.RangingResultPosition.toPositionResult(): UwbResult = UwbResult.PositionResult(
+    distance = position.distance,
+    azimuth = position.azimuth,
+    elevation = position.elevation,
+    elapsedRealtimeNanos = position.elapsedRealtimeNanos
+)
