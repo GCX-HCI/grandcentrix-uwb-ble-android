5a768e2b0ec0022f36ef2e77f4ba4efb394e05be
bjoern.kinberger <bjoern.kinberger@grandcentrix.net>
2024-05-22 17:12:48 +0200

    [documentation] - add documentation

--- lib/src/main/java/net/grandcentrix/lib/uwb/model/DeviceConfig.kt
+++ lib/src/main/java/net/grandcentrix/lib/uwb/model/DeviceConfig.kt
@@ -1,5 +1,10 @@
 package net.grandcentrix.lib.uwb.model
 
+/**
+ * This open class serves as a base configuration for uwb devices that function as a controller.
+ *
+ * @property deviceMacAddress The MAC address of the device in byte array format.
+ */
 open class DeviceConfig(
     open val deviceMacAddress: ByteArray
 )
--- lib/src/main/java/net/grandcentrix/lib/uwb/model/PhoneConfig.kt
+++ lib/src/main/java/net/grandcentrix/lib/uwb/model/PhoneConfig.kt
@@ -1,5 +1,14 @@
 package net.grandcentrix.lib.uwb.model
 
+/**
+ * This open class serves as a base configuration for a phone that function as a controlee.
+ *
+ * @property sessionId The session identifier for the ranging operation (integer).
+ * @property preambleIndex The preamble index used for transmission (byte).
+ * @property channel The channel to be used for communication (byte).
+ * @property phoneAddress The phone's address in byte array format.
+ */
+
 open class PhoneConfig(
     open val sessionId: Int,
     open val preambleIndex: Byte,
